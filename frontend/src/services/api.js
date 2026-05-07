77feddf2224c3a24389cab7b213adcb98b397e8e
Nikhil <nikhiltanneru9@gmail.com>
2026-05-07 16:25:17 +0530

    updated production api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+
+  baseURL:
+    "https://task-management-system-production-88e3.up.railway.app/api",
+
 });
 
 export default API;
